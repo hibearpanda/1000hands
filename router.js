44c155138a5d2b0f06cad2fb0395dfcd01ac2c47
minocys <harich.leung@gmail.com>
2015-11-16 14:26:02 +0800

    Change to Mailgun email service

--- router.js
+++ router.js
@@ -1,14 +1,20 @@
 var router = require('express').Router();
 var path = require('path');
-
 var nodemailer = require('nodemailer');
-var transporter = nodemailer.createTransport({
-  service: 'gmail',
+var mailgun = require('nodemailer-mailgun-transport');
+
+if (!process.env.MY_EMAIL) {
+  var config = require('./config');
+}
+
+var auth = {
   auth: {
-    user: 'ryan@divedecks.com',
-    pass: ''
+    api_key: process.env.MAILGUN_API || config.MAILGUN_API,
+    domain: process.env.MAILGUN_DOMAIN || config.MAILGUN_DOMAIN
   }
-});
+};
+
+var transporter = nodemailer.createTransport(mailgun(auth));
 
 router.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname, '/public/', req.url));
@@ -16,18 +22,17 @@ router.get('/*', function(req, res) {
 
 router.post('/contact', function(req, res) {
   var message = {
-    from: req.body.name + '&lt' + req.body.email + '&gt',
+    from: req.body.email,
     to: 'ryan@divedecks.com',
-    subject: 'Message from Website - ' + req.body.subject,
+    subject: 'Message from - ' + req.body.name,
     text: req.body.text
   };
 
   transporter.sendMail(message, function(err, info) {
     if(err) {
       console.log('error');
-      res.json({succes: false});
+      res.json({success: false});
       console.log(err);
-      console.log('error');
     } else {
       res.json({success: true});
     }
