9a3f1931030451e86a932540650f02ca76efd03b
minocys <harich.leung@gmail.com>
2015-11-18 17:43:56 +0800

    Define password-check route

--- server/router.js
+++ server/router.js
@@ -2,10 +2,12 @@ var router = require('express').Router();
 var path = require('path');
 var mail = require('./mail');
 
+// General page delivery
 router.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname, '/public/', req.url));
 });
 
+// Contact form route - get account details from admin
 router.post('/contact', function(req, res) {
   var message = {
     from: req.body.email,
@@ -23,6 +25,15 @@ router.post('/contact', function(req, res) {
       res.json({success: true});
     }
   });
+});
+
+// Behind the Scenes password route
+// TODO: change password check to process.env variable
+router.post('/backer_login', function(req, res) {
+  if (req.body.password !== 'test') {
+    res.json({success: false});
+  }
+  res.json({success: true});
 });
 
 module.exports = router;
