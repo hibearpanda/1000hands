04c647d787887e32a1f3e1b507ab6a522c5bb560
minocys <harich.leung@gmail.com>
2015-11-18 18:34:53 +0800

    Add backerlogin html framework

--- public/js/backerLogin.js
+++ public/js/backerLogin.js
@@ -1,18 +1,18 @@
 // Attaches event listeners for behind the scenes page
 // Checks password, reveals bonus content on success
 module.exports.attach = function() {
-  var $passwordField = $('.passwordField');
-  if($passwordField) {
-    $passwordField.submit(function(event) {
+  var $passwordForm = $('.passwordForm');
+  if($passwordForm) {
+    $passwordForm.submit(function(event) {
       event.preventDefault();
 
       $.ajax({
         type: 'post',
         url: '/backer_login',
-        data: JSON.stringify({ password: $passwordField.val() }),
+        data: JSON.stringify({ password: $('#passwordField').val() }),
         contentType: 'application/json',
         success: function(data) {
-          $passwordField.hide();
+          $passwordForm.hide();
           $('.backer-content').show();
         },
         dataType: 'json'
